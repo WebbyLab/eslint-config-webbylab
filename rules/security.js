942de4a505f409cde06d25b4a99dede91471967c
Viktor Turskyi <koorchik@gmail.com>
2020-05-09 11:40:58 +0300

    Disable detect-non-literal-fs-filename

--- rules/security.js
+++ rules/security.js
@@ -8,7 +8,6 @@ module.exports = {
         'security/detect-buffer-noassert': 'error',
         'security/detect-child-process': 'error',
         'security/detect-eval-with-expression': 'error',
-        'security/detect-non-literal-fs-filename': 'error',
         'security/detect-non-literal-regexp': 'error',
         'security/detect-possible-timing-attacks': 'error',
         'security/detect-pseudoRandomBytes': 'error'
